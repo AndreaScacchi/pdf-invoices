5e6b27c0c03bc5c6d4cb0e6d7b413695278cc894
AndreaScacchi <andreascacchi10@gmail.com>
2023-09-09 16:55:30 +0200

    header and rows added

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 # I've used the for loop to loop through all the file in the invoices folder and generated the same number of PDF files
 for filepath in filepaths:
-    # here I've used the pandas library to read the files
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
     
     # with the fpdf library I setted the orientation, unit and format of the files
     pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -24,7 +22,31 @@ for filepath in filepaths:
     
     # here instead I've done the same for the date
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+    
+    # here I've used the pandas library to read the files
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    
+    # add header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=9, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+    
+    # add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=8)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
     
     # this line generates the PDFs
     pdf.output(f"PDFs/{filename}.pdf")
